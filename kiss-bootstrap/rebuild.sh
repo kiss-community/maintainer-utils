7c1675fce069d5f4766005b9eaa77934c6d05b3d
git-bruh <e817509a-8ee9-4332-b0ad-3a6bdf9ab63f@aleeas.com>
2022-10-03 16:26:09 +0530

    kiss-bootstrap/rebuild.sh: cleanup unused packages

--- kiss-bootstrap/rebuild.sh
+++ kiss-bootstrap/rebuild.sh
@@ -2,7 +2,21 @@
 
 set -eu
 
+get_depends() {
+	echo "$1"
+
+	DEPFILE="/var/db/kiss/installed/$1/depends"
+
+	[ -f "$DEPFILE" ] || return
+
+	while read -r dep _; do
+    	get_depends "$dep"
+    done < "$DEPFILE"
+}
+
 DUMMY_PACKAGE_DIR="/tmp/pkg/__dummy"
+FINAL_PKGS_FILE="/tmp/depends"
+
 export KISS_PATH="${DUMMY_PACKAGE_DIR%/*}"
 
 cd /var/db/kiss/installed
@@ -13,3 +27,18 @@ kiss new "$DUMMY_PACKAGE_DIR" 1
 printf '%s\n' "$@" > "$DUMMY_PACKAGE_DIR/depends"
 
 KISS_PROMPT=0 kiss build "${DUMMY_PACKAGE_DIR##*/}" "$@"
+
+final="$(
+	while read -r pkg; do
+		get_depends "$pkg"
+	done < "$FINAL_PKGS_FILE" | sort -u
+)"
+
+# Remove unneeded packages, such as make deps
+for pkg; do
+	for search in $final; do
+    	[ "$search" = "$pkg" ] && continue 2
+	done
+
+	KISS_FORCE=1 kiss remove "$pkg"
+done
